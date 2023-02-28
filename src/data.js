c226cd1eb0e6ec8d77df1f2db99850e57bde0ffd
Touheed Razaq <trkraza0204@gmail.com>
2023-02-28 14:33:11 -0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+      id: 1,
+      name: 'Monika',
+      age: 29,
+      image:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+    },
+    {
+      id: 2,
+      name: 'Harika',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Hari',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Trk',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Shalu',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+    {
+      id: 6,
+      name: 'Mathai',
+      age: 19,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
